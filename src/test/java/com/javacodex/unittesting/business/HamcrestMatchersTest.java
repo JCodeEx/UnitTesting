673f57e9be8f3dcc2db033350c8e2d0b4400cc65
rasmin <rasminranjangiri8@gmail.com>
2026-01-27 12:10:36 +0530

    learnHamcrest......

---
+++ src/test/java/com/javacodex/unittesting/business/HamcrestMatchersTest.java
@@ -0,0 +1,21 @@
+package com.javacodex.unittesting.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class HamcrestMatchersTest {
+    @Test
+public void learnHamcrest(){
+
+        List<Integer> numbers= Arrays.asList(12,23,45,50);
+
+        assertThat(numbers,hasSize(4));
+        assertThat(numbers, hasItem(23));
+        assertThat(numbers, everyItem(greaterThan(10)));
+        assertThat(numbers, contains(12, 23, 45, 50));
+}
+
+}
